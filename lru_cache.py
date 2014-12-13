1475405957b4838c6240fcbc94f6e2ed3bc94241
Sahil <kumar.sahil@gmail.com>
2014-12-13 00:03:00 -0800

    Updated LRU implementation to utilize built-in OrderedDict

--- lru_cache.py
+++ lru_cache.py
@@ -1,50 +1,38 @@
 # Run Tests:
 # python -m unittest tests.unit_tests
 # from the root of the repository
-
+from collections import OrderedDict
 class LRUCache():
 
     # @param capacity, an integer
     def __init__(self, capacity):
         self.__capacity = capacity
-        self.__usage = []
-        self.__store = {}
+        self.__store = OrderedDict()
 
+    # @param key
     # @return an object
     def get(self, key):
-        val = self.__store.get(key)
-        # update usage order
-        if val:
-            self.__update_usage(key) 
-        else:
-            val = None
-        return val
-    
-    # @param key
-    def __update_usage(self, key):
         try:
-            idx = self.__usage.index(key)
-            self.__usage = self.__usage[0:idx] + self.__usage[idx+1:]
+            val = self.__store.pop(key)
+            # update usage order
+            self.__store[key] = val
+            return val
         except:
-            pass
-        self.__usage.insert(0, key)
+            return None
 
     # @param key, an integer
     # @param value, an integer
     # @return nothing
     def set(self, key, value):
-        if not self.__store.get(key) and len(self.__store) == self.__capacity:
-            # make room for 1
-            purged = self.__usage.pop()
-            try:
-                self.__store.pop(purged)
-            except:
-                pass
+        try:
+            self.__store.pop(key)
+        except:
+            pass
         self.__store[key] = value
-        self.__update_usage(key)
-        
+        while(len(self.__store) > self.__capacity):
+            self.__store.popitem(last=False)
+
     # Clears the cache
     def clear(self):
-        self.__store = {}
-        self.__usage = []
-        
+        self.__store = OrderedDict()
+
